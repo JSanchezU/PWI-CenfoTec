26d06595b4f0b6d82c1f5c682dd5ebc97d4ec466
Javierimperial <javiersanchezugalde@gmail.com>
2015-12-15 00:19:56 -0600

    Polymer 17

---
+++ 17-ReusablePolymer/controllers.js
@@ -0,0 +1,21 @@
+angular.module('polymer.controllers', [])
+
+.controller('HomeController', ['$scope' ,function($scope){
+	
+}])
+
+.controller('ProductsController', [function(){
+
+}])
+
+.controller('VideosController', ['$scope', function($scope){
+	var video = document.querySelector("video");
+
+	$scope.play = function(){
+		video.play();
+	};
+
+	$scope.pause = function(){
+		video.pause();
+	};
+}]);
